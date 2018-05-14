ea07ae1ed8e98950b0e4f27773080dd09a6d9034
gdcruz9 <33048285+gdcruz9@users.noreply.github.com>
2018-05-14 00:14:35 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,15 +20,6 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule} from 'angularfire2/auth';
 
-export const firebaseConfig = {
-apiKey: "AIzaSyB_O6-t7pGgpg1_5JHgEip6_u1ITFCCs34",
-authDomain: "proyecto-final-142ae.firebaseapp.com",
-databaseURL: "https://proyecto-final-142ae.firebaseio.com",
-projectId: "proyecto-final-142ae",
-storageBucket: "proyecto-final-142ae.appspot.com",
-messagingSenderId: "426362984806"
-};
-
 firebase.initializeApp(firebaseConfig);
 
 @NgModule({
